e8462bdd2166ac74d4728164a73f036c73508892
Louis Burke <louis.j.burke@gmail.com>
2015-12-12 17:46:17 +0000

    Yo

--- src/webdev/core.clj
+++ src/webdev/core.clj
@@ -2,7 +2,8 @@
   (:require [ring.adapter.jetty :as jetty]
             [ring.middleware.reload :refer [wrap-reload]]
             [compojure.core :refer [defroutes GET]]
-            [compojure.route :refer [not-found]]))
+            [compojure.route :refer [not-found]]
+            [ring.handler.dump :refer [handle-dump]]))
 
 (defn greet [req]
   {:status 200
@@ -13,14 +14,25 @@
   {:status 200
    :body "Goodbye, Fuck heads!"
    :headers {}})
-    ;;:else
-    ;;{:status 404
-    ;; :body "Page not found"
-    ;; :headers {}}))
+
+(defn about [req]
+  {:status 200
+   :body "My name is Lou! How do you do?!"
+   :headers {}})
+
+(defn yo [req]
+  (let [name (get-in req [:route-params :name])]
+  {:status 200
+   :body (str "Yo! " name "!")
+   :headers {}}))
 
 (defroutes app
   (GET "/" [] greet)
   (GET "/goodbye" [] goodbye)
+  (GET "/yo/:name" [] yo)
+
+  (GET "/about" [] about)
+  (GET "/request" [] handle-dump)
   (not-found "Page not found."))
 
 (defn -main [port]
